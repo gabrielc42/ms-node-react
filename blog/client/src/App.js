3ea64a6ea1f4b32e993be0b13761da778d7d675b
gabrielc42 <clark.gabe97@gmail.com>
2023-03-13 18:20:33 -0500

    building post submission

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
-     
+import PostCreate from "./PostCreate"
+
 const App = () => {
-  return <div>h3ll0o</div>;
+  return (
+    <div className="container">
+      <h1>h3ll0o</h1>
+      <PostCreate />
+    </div>
+  );
 };
- 
+
 export default App;
