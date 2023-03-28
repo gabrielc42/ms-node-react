d9b6dee0d9371a207ecb6a30a93b9f7c0df8de72
gabrielc42 <clark.gabe97@gmail.com>
2023-03-28 14:28:33 -0500

    fetching and rendering posts

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import PostCreate from "./PostCreate"
+import PostCreate from "./PostCreate";
+import PostList from './PostList';
 
 const App = () => {
   return (
     <div className="container">
-      <h1>h3ll0o</h1>
+      <h1>h3ll0o :D Create a post!</h1>
       <PostCreate />
+      <hr />
+      <h1>Postz</h1>
+      <PostList />
     </div>
   );
 };
